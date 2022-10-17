d91b86f860b7287b79324e0fc85a20b19181c3c2
Owen <o@oclyke.dev>
2022-10-16 22:05:04 -0700

    add cumulative test diagnostics

--- tools/run-tests.sh
+++ tools/run-tests.sh
@@ -6,13 +6,31 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ret=0
 source=$(find $SCRIPT_DIR/.. -regex '.*\/build\/test_[^\.]*$')
 
+echo "$source"
+
+count=0
+for test in $source
+do
+  let "count+=1"
+done
+echo ""
+echo "$count tests discovered"
+echo ""
+echo ""
+
+run=0
+failed=0
 for test in $source
 do
   eval $test
   status=$?
+  let "run+=1"
   if [ $status -ne 0 ]; then
     ret=$status
+    let "failed+=1"
     echo "$test FAILED"
   fi
 done
+echo ""
+echo "$run tests run, $failed failed"
 exit $ret
