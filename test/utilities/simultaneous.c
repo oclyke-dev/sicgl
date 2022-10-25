bb0daf6566e865fa6cdce516829832f967d808de
Owen <o@oclyke.dev>
2022-10-25 13:14:58 -0700

    add display region test

--- test/utilities/simultaneous.c
+++ test/utilities/simultaneous.c
@@ -32,3 +32,14 @@ int simultaneous_specific_display_vline(
 out:
   return ret;
 }
+
+int simultaneous_specific_display_region(
+    gdImage* image, specific_interface_t* interface, color_sequence_t* sequence,
+    ext_t u0, ext_t v0, ext_t u1, ext_t v1) {
+  int ret = 0;
+  color_t color = color_sequence_get_color(sequence);
+  gdImageFilledRectangle(image, u0, v0, u1, v1, *(int*)color);
+  sicgl_specific_region(interface, sequence, u0, v0, u1, v1);
+out:
+  return ret;
+}
