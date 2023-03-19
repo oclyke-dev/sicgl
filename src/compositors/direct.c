16fbec346ae814f7d72ca424c88ea4a448b9ed7b
Owen <o@oclyke.dev>
2023-03-19 13:55:54 -0700

    Update direct.c

--- src/compositors/direct.c
+++ src/compositors/direct.c
@@ -1,17 +1,14 @@
 #include "sicgl/compositors.h"
+#include <string.h>
 
 void compositor_direct_set(
     color_t* source, color_t* destination, size_t width, void* args) {
-  for (size_t idx = 0; idx < width; idx++) {
-    destination[idx] = source[idx];
-  }
+  memcpy(destination, source, width * bytes_per_pixel());
 }
 
 void compositor_direct_clear(
     color_t* source, color_t* destination, size_t width, void* args) {
-  for (size_t idx = 0; idx < width; idx++) {
-    destination[idx] = 0x00;
-  }
+  memset(destination, 0x00, width * bytes_per_pixel());
 }
 
 void compositor_direct_none(
