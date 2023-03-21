046799d1cc9615792f2250ac3a9e498dd5cf9cd9
Owen <o@oclyke.dev>
2023-03-20 23:05:07 -0700

    add gradient alpha sources

--- test/tests/compositors/main.c
+++ test/tests/compositors/main.c
@@ -16,6 +16,9 @@ screen_t* screen = NULL;
 interface_t* backdrop_interface = NULL;
 interface_t* source_interface = NULL;
 
+interface_t* gradient_backdrop_interface = NULL;
+interface_t* gradient_source_interface = NULL;
+
 color_t clear = gdTrueColorAlpha(0, 0, 0, gdAlphaTransparent);
 color_t canary = gdTrueColorAlpha(255, 255, 0, gdAlphaOpaque);
 color_t cyan = gdTrueColorAlpha(0, 255, 255, gdAlphaOpaque);
@@ -25,11 +28,17 @@ void setUp(void) {
   screen = new_screen_extent(width, height, 0, 0);
   backdrop_interface = new_libgd_interface(screen, NULL, 0);
   source_interface = new_libgd_interface(screen, NULL, 0);
+  gradient_backdrop_interface = new_libgd_interface(screen, NULL, 0);
+  gradient_source_interface = new_libgd_interface(screen, NULL, 0);
   TEST_ASSERT_NOT_NULL_MESSAGE(screen, "could not create screen");
   TEST_ASSERT_NOT_NULL_MESSAGE(
       backdrop_interface, "could not create backdrop interface");
   TEST_ASSERT_NOT_NULL_MESSAGE(
       source_interface, "could not create source interface");
+  TEST_ASSERT_NOT_NULL_MESSAGE(
+      gradient_backdrop_interface, "could not create gradient backdrop interface");
+  TEST_ASSERT_NOT_NULL_MESSAGE(
+      gradient_source_interface, "could not create gradient source interface");
 
   // draw rectangles
   sicgl_interface_rectangle_filled(
@@ -41,9 +50,20 @@ void setUp(void) {
       source_interface, clear, 0, 0, width - 1, height - 1);
   sicgl_interface_rectangle_filled(source_interface, cyan, 75, 75, 225, 225);
 
+  // draw gradients
+  for (ext_t idx = 0; idx < width; idx++) {
+    const int alpha = (idx << 23);
+    for (ext_t idy = 0; idy < height; idy++) {
+        sicgl_interface_line(gradient_backdrop_interface, (alpha & 0xff000000) | 0x00ff0000, 0, idx, width - 1, idx);
+        sicgl_interface_line(gradient_source_interface, (alpha & 0xff000000) | 0x000000ff, idx, 0, idx, height - 1);
+    }    
+  }
+
   // save the reference pictures
   png_t* backdrop_png = new_png_from_libgd_interface(backdrop_interface);
   png_t* source_png = new_png_from_libgd_interface(source_interface);
+  png_t* gradient_backdrop_png = new_png_from_libgd_interface(gradient_backdrop_interface);
+  png_t* gradient_source_png = new_png_from_libgd_interface(gradient_source_interface);
 
   TEST_ASSERT_NOT_NULL_MESSAGE(backdrop_png, "could not create backdrop png");
   TEST_ASSERT_NOT_NULL_MESSAGE(source_png, "could not create source png");
@@ -52,6 +72,10 @@ void setUp(void) {
       0, png_to_file(backdrop_png, TEST_OUTPUT_DIR "/_backdrop.png"));
   TEST_ASSERT_EQUAL_INT(
       0, png_to_file(source_png, TEST_OUTPUT_DIR "/_source.png"));
+  TEST_ASSERT_EQUAL_INT(
+      0, png_to_file(gradient_backdrop_png, TEST_OUTPUT_DIR "/_gradient_backdrop.png"));
+  TEST_ASSERT_EQUAL_INT(
+      0, png_to_file(gradient_source_png, TEST_OUTPUT_DIR "/_gradient_source.png"));
 }
 
 void tearDown(void) {
