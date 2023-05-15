2f882c977ef3ee06c53234b5fa2280e1293d7dd9
Owen <o@oclyke.dev>
2023-05-15 10:42:06 -0700

    add references for all compositors

--- test/tests/compositors/src/direct.c
+++ test/tests/compositors/src/direct.c
@@ -2,6 +2,12 @@
 #include "tests/utilities.h"
 #include "utils.h"
 
-DEFINE_UNREFERENCED_COMPOSITOR_TEST(direct_set, compositor_direct_set)
-DEFINE_UNREFERENCED_COMPOSITOR_TEST(direct_clear, compositor_direct_clear)
-DEFINE_UNREFERENCED_COMPOSITOR_TEST(direct_none, compositor_direct_none)
+DEFINE_REFERENCED_COMPOSITOR_TEST(
+    direct_set, compositor_direct_set, "solid_direct_set.png",
+    "gradient_direct_set.png")
+DEFINE_REFERENCED_COMPOSITOR_TEST(
+    direct_clear, compositor_direct_clear, "solid_direct_clear.png",
+    "gradient_direct_clear.png")
+DEFINE_REFERENCED_COMPOSITOR_TEST(
+    direct_none, compositor_direct_none, "solid_direct_none.png",
+    "gradient_direct_none.png")
