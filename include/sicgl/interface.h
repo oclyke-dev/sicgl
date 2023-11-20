deb691ed75e221c840d11bd2915637f771efc60b
Owen <oclyke@oclyke.dev>
2023-11-19 22:32:42 -0800

    pixel getters (#65)
    * add pixel getters
    
    * update
    
    * format

--- include/sicgl/interface.h
+++ include/sicgl/interface.h
@@ -12,3 +12,8 @@ typedef struct _interface_t {
   color_t* memory;   // pointer to start of specific memory
   size_t length;     // memory length in pixels
 } interface_t;
+
+int sicgl_interface_get_pixel_offset(
+    interface_t* interface, uext_t offset, color_t* color);
+int sicgl_interface_get_pixel(
+    interface_t* interface, uext_t u, uext_t v, color_t* color);
